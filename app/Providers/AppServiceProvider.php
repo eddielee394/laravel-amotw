63b26e5875fa71e37e727a147382299148ec8dda
Eddie Padin <eddie@entertainermedia.net>
2018-11-07 17:24:33 -0600

    core:  add dependencies; - add ide-helper; - updated gitignore; - add check to prevent loading of ide-helper on production env's;

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        //Ide helper
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(
+                \Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class
+            );
+        }
     }
 }
