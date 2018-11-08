f0476f00db80efcc47c8735cb649b269cb8e73cb
Eddie Padin <eddie@entertainermedia.net>
2018-11-07 19:10:09 -0600

    feat:  setup crud scaffolding; - add base controller; - update RouteServiceProvider to prefix and namespace api routes; - add crud generator config;

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,8 +38,6 @@ class RouteServiceProvider extends ServiceProvider
         $this->mapApiRoutes();
 
         $this->mapWebRoutes();
-
-        //
     }
 
     /**
@@ -52,8 +50,8 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapWebRoutes()
     {
         Route::middleware('web')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
+            ->namespace($this->namespace)
+            ->group(base_path('routes/web.php'));
     }
 
     /**
@@ -65,9 +63,11 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
+        //Add route prefix & namespace suffix for api routes
         Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+            ->middleware('api')
+            ->as('api.')
+            ->namespace($this->namespace . "\\API")
+            ->group(base_path('routes/api.php'));
     }
 }
